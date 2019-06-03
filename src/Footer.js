eefdc5708a3d204a82c760ac9b3cd5c3b8ba9c61
zhurankou <s.zhurankou@gmail.com>
2019-06-02 18:37:04 -0700

    Update About

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () =>
   <div className='footer'>
-    <p>&copy; 2019 Vets Who Design by <a href='https://twitter.com/zhurankou'>@zhurankou</a></p>
+    <p>&copy; 2019 Vets Who Design by <a href='https://twitter.com/zhurankou' target='_blank' rel="noopener noreferrer">@zhurankou</a></p>
   </div>
 
 export default Footer;
