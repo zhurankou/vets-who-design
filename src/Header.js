c065d7c128b35ec5eaba9c4057ce1bdca6e99773
zhurankou <s.zhurankou@gmail.com>
2019-06-06 15:17:11 -0700

    Update Header

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,7 @@ const Header = () =>
       <div className='menu-links'>
         <Link to='/about'>About</Link>
         <Link to='/stories'>Stories</Link>
-        <a href='https://airtable.com/shrrLfheMQ9CEJP7V' target='_blank' rel="noopener noreferrer">Nominate</a>
+        <a href='https://airtable.com/shrWtohVYk4W7lDYp' target='_blank' rel="noopener noreferrer">Nominate</a>
         <a href='https://join.slack.com/t/vetswhodesign/shared_invite/enQtNjIzNjgxNzU5ODg4LWI0MDg4ZjVlNTgwYzVmMDE4NTIyYTE1YWNjNTMyODgzZmZjNWZhZjExZjljMzE2ZDgyNjYxYjRmYjAxYThhYjU' target='_blank' rel="noopener noreferrer">Slack</a>
         <a href='https://teespring.com/VetsWhoDesign' target='_blank' rel="noopener noreferrer">Uniform</a>
       </div>
